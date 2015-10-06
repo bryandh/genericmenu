1194a6e5b9dbe8ebe0f66505a0db4f6ef3191886
bryandh <bryandh1@gmail.com>
2015-10-06 20:06:38 +0200

    Stream and Lambdas removed to improve speed

--- application/src/core/GenericMenu.java
+++ application/src/core/GenericMenu.java
@@ -18,10 +18,12 @@ public class GenericMenu {
 	}
 
 	/**
-	 * Adds a MenuItem with only a key and a name to the menuItems ArrayList 
+	 * Adds a MenuItem with only a key and a name to the menuItems ArrayList
 	 * 
-	 * @param key	the key the menu item will belong to
-	 * @param name	the name of the menu item
+	 * @param key
+	 *            the key the menu item will belong to
+	 * @param name
+	 *            the name of the menu item
 	 */
 	private void addMenuItem(String key, String name) {
 		MenuItem menuItem = new MenuItem(key, name);
@@ -31,17 +33,21 @@ public class GenericMenu {
 	/**
 	 * Adds a MenuItem with key, name and runnable to the menuItems ArrayList
 	 * 
-	 * @param key		the key the menu item will belong to
-	 * @param name		the name of the menu item
-	 * @param runnable	the method that will be run when the menu item is chosen
+	 * @param key
+	 *            the key the menu item will belong to
+	 * @param name
+	 *            the name of the menu item
+	 * @param runnable
+	 *            the method that will be run when the menu item is chosen
 	 */
 	public void addMenuItem(String key, String name, Runnable runnable) {
 		MenuItem menuItem = new MenuItem(key, name, runnable);
 		menuItems.add(menuItem);
 	}
-	
+
 	/**
-	 * Prints all the existing menu items in the console in the format [key]: name
+	 * Prints all the existing menu items in the console in the format [key]:
+	 * name
 	 */
 	public void printMenuItems() {
 		for (MenuItem menuItem : menuItems) {
@@ -52,20 +58,28 @@ public class GenericMenu {
 	/**
 	 * Runs the runnable of the menu item that matches the given key
 	 * 
-	 * @param key			key of the menu item to run the runnable of
-	 * @throws Exception	exception when there is no matching menu item found for the given key
+	 * @param key
+	 *            key of the menu item to run the runnable of
+	 * @throws Exception
+	 *             exception when there is no matching menu item found for the
+	 *             given key
 	 */
-	public void runCommand(String key) throws Exception {
-		// filter the menu item(s) with the corresponding key
-		Stream<MenuItem> filteredMenuItemsStream = menuItems.stream().filter(i -> i.getKey().equals(key));
+	public void runCommand2(String key) throws Exception {
+		ArrayList<MenuItem> filteredMenuItems = new ArrayList<MenuItem>();
 		
-		// convert stream to ArrayList
-		ArrayList<MenuItem> filteredMenuItems = (ArrayList<MenuItem>) filteredMenuItemsStream.collect(Collectors.toList());
-		
-		if(filteredMenuItems.size() > 0)
-			// if there are any menu items with the given key, run their runnables
-			filteredMenuItems.forEach(i -> i.getRunnable().run());
-		else
+		// filter through the menu items, checking if the given key corresponds to a MenuItem
+		for (MenuItem i : menuItems) {
+			if (i.getKey().equals(key))
+				filteredMenuItems.add(i);
+		}
+
+		if (filteredMenuItems.size() > 0) {
+			// if there are any menu items with the given key, run their
+			// runnables
+			for (MenuItem i : filteredMenuItems) {
+				i.getRunnable().run();
+			}
+		} else
 			// if not, throw an exception that the key doesn't exist
 			throw new Exception("No valid option for '" + key + "' found, try again.");
 	}
@@ -81,16 +95,17 @@ public class GenericMenu {
 	}
 
 	/**
-	 * Adds the default menu items, for example the quit menu item should always be in there
-	 * Beware: default menu items should always be added to the input switch
+	 * Adds the default menu items, for example the quit menu item should always
+	 * be in there Beware: default menu items should always be added to the
+	 * input switch
 	 */
 	public void addDefaultMenuItems() {
 		addMenuItem("Q", "Quit");
 	}
-	
-	public void initMenu(){
+
+	public void initMenu() {
 		addDefaultMenuItems();
-		
+
 		Boolean quit = false;
 
 		while (!quit) {
